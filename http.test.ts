40efb2b922675975e8b53ab8c6c92f9f3f0f1a84
Daniel <dan@dgr.dev>
2020-12-24 03:02:24 -0600

    fix: remove unnecessary line, add application-type to test

--- http.test.ts
+++ http.test.ts
@@ -2,13 +2,12 @@ import { router } from "./api/routes.ts";
 import { Application } from "./deps.ts";
 import { assertStrictEquals, assertObjectMatch, axiosd } from "./dev_deps.ts";
 
-Deno.test("/route returns correct status and data", async () => {
+Deno.test("/route returns correct status, type, and data", async () => {
   // Arrange
   const app = new Application();
   const controller = new AbortController();
   const { signal } = controller;
   app.use(router.routes());
-  app.use(router.allowedMethods());
   const listenPromise = app.listen({ port: 8001, signal });
   // Act
   const response = await axiosd.get("http://localhost:8001/route");
@@ -17,6 +16,10 @@ Deno.test("/route returns correct status and data", async () => {
   assertObjectMatch(response.data, {
     hello: { world: "example" },
   });
+  assertStrictEquals(
+    response.headers.get("content-type"),
+    "application/json; charset=utf-8"
+  );
   // Cleanup
   controller.abort();
   await listenPromise;
